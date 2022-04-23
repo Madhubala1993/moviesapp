adbf6fa0ea1bf60df4f03fca5963c254a03860da
Madhubala1993 <madhu.a1993@gmail.com>
2022-04-23 12:34:01 +0530

    final commit

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-// export const API = "https://moviesapp-2022.herokuapp.com";
+export const API = "https://moviesapp-2022.herokuapp.com";
 // export const API = "https://61e688bcce3a2d0017359229.mockapi.io";
 //
-export const API = "http://localhost:9023";
+// export const API = "http://localhost:9023";
